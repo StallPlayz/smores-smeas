bbf7143c51ee3d67f7892dc370325e4f21ee650c
Stall Gaming <stallplayz123@gmail.com>
2026-02-13 09:18:55 +0700

    Clean up: keeping only Next.js files

--- smores-smeas-api/routes/api.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ProductController;
-use App\Http\Controllers\OrderController;
-
-Route::get('/products', action: [ProductController::class, 'index']);
-Route::post('/checkout', [OrderController::class, 'store']);
