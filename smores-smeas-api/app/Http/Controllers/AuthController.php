e040a299aac0765e4819441d34c585dadcf07cc1
Dava Galang Samudra <davagalangsamudra06@gmail.com>
2026-03-12 11:43:58 +0700

    Migration, Controller logic and handling api endpoint.

---
+++ smores-smeas-api/app/Http/Controllers/AuthController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    /**
+     * Login user
+     */
+
+    public function login(Request $request)
+    {
+        $credentials = $request->validate([
+            'email' => 'required|email',
+            'password' => 'required'
+        ]);
+
+        if (!Auth::attempt($credentials)) {
+            return response()->json([
+                'message' => 'Invalid credentials'
+            ], 401);
+        }
+
+        $user = Auth::user();
+
+        $token = $user->createToken('api-token')->plainTextToken;
+
+        return response()->json([
+            'user' => $user,
+            'token' => $token
+        ]);
+    }
+
+    /**
+     * Logout user
+     */
+    public function logout(Request $request)
+    {
+        $request->user()->tokens()->delete();
+
+        return response()->json([
+            'message' => 'You have been logged out'
+        ]);
+    }
+}
--- smores-smeas-api/routes/api.php
+++ smores-smeas-api/routes/api.php
@@ -1,8 +1,21 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\MessageController;
 use App\Http\Controllers\ProductController;
-use App\Http\Controllers\OrderController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
 
-Route::get('/products', action: [ProductController::class, 'index']);
-Route::post('/checkout', [OrderController::class, 'store']);
+
+// Auth routes
+Route::prefix('auth')->group(function () {
+    Route::post('/login', [AuthController::class, 'login']);
+    Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
+});
+
+// Resources
+Route::apiResource('/products', ProductController::class);
+Route::apiResource('/products', ProductController::class)->middleware('auth:sanctum')->only(['store', 'update', 'destroy']);
+
+Route::apiResource('/messages', MessageController::class);
+Route::apiResource('/messages', MessageController::class)->middleware('auth:sanctum')->only(['index', 'destroy', 'update']);
